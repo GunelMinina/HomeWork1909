a9f8ef99f0b35622e000052550804dbec7742aa0
Gunel <gunelminina@gmail.com>
2022-09-20 23:37:17 +0300

    Zadacha01

---
+++ Zadacha015_DniNedeli/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Vvedite chislo, oboznachaushee den nedeli : ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+    Console.WriteLine($"{a} vichodnoy den'");
+}
+else if (a >= 1 && a <= 5)
+{
+    Console.WriteLine($"{a} budniy den'");
+}
+else
+{
+    Console.WriteLine("nekorrektnoe chislo");
+}
