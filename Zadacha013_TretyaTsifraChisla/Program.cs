48a32268112ef57c593da8424b720cd38e2e77e1
Gunel <gunelminina@gmail.com>
2022-09-20 23:08:04 +0300

    Zadacha013

---
+++ Zadacha013_TretyaTsifraChisla/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Vvedite chislo : ");
+string a = Console.ReadLine();
+
+if (int.Parse(a) < 100)
+{
+    Console.WriteLine("Tret'ey tsifry net");
+}
+else
+{
+    Console.WriteLine($"Tret'ya tsifra {a[2]}");
+}
